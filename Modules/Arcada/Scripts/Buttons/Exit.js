e50b0291f06addea086d386ed1cff325d37c270d
Tsbrys <126396764+Tsbrys@users.noreply.github.com>
2025-04-12 13:09:39 +0300

    Update Exit.js

--- Modules/Arcada/Scripts/Buttons/Exit.js
+++ Modules/Arcada/Scripts/Buttons/Exit.js
@@ -1,1 +1,17 @@
+document.getElementById('exit-btn').addEventListener('click', function() {
+    let currentPath = window.location.pathname;
+    let targetUrl;
 
+    if (currentPath.includes('/Game-Hub/')) {
+        // Якщо на GitHub Pages
+        targetUrl = 'https://tsbrys.github.io/Game-Hub/';
+    } else {
+        // Якщо локально
+        targetUrl = 'index.html';
+    }
+
+    window.location.href = targetUrl;
+
+    // Закриття вкладки (може не спрацювати в більшості браузерів через безпеку)
+    window.close();
+});
